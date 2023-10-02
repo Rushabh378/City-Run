f661f1decc9cdd259ea5dfdba6635766ae98ec51
Rushabh Gohel <103501258+Rushabh378@users.noreply.github.com>
2023-10-02 15:23:29 +0530

    made some changes.

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
@@ -8,9 +5,7 @@ public class PlayerController : MonoBehaviour
 {
     private CharacterController controller;
     private float yVelocity = 0;
-    private bool onGround = true;
     [SerializeField] private Animator animator;
-    [SerializeField] private BoxCollider groundChecker;
     [SerializeField] private float speed = 5f;
     [SerializeField] private float jumpForce = 15f;
     [SerializeField] private float gravity = 1f;
@@ -24,26 +19,17 @@ public class PlayerController : MonoBehaviour
         Move();
     }
 
-    public void OnTriggerEnter(Collider other)
-    {
-        onGround = true;
-    }
-    public void OnTriggerExit(Collider other)
-    {
-        onGround = false;
-    }
-
     private void Move()
     {
        
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 
-        if (onGround)
+        if (controller.isGrounded)
         {
             if (Input.GetButtonDown("Jump"))
             {
                 yVelocity = jumpForce;
-            } 
+            }
         }
         else
         {
@@ -53,6 +39,7 @@ public class PlayerController : MonoBehaviour
         move.y = yVelocity;
 
         controller.Move(move * Time.deltaTime * speed);
+
 
         if(move != Vector3.zero)
         {
